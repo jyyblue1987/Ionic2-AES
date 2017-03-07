924aa2720ef127347f8afc39b968e7f0f1888fa5
jyyblue <jyyblue1987@outlook.com>
2017-03-07 10:35:49 +0800

    [add] make AES encrypt and decrypt with hex key and initial vector.

--- AESTest/src/pages/home/home.ts
+++ AESTest/src/pages/home/home.ts
@@ -1,15 +1,57 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	key: any;
+	iv: any;
+	plain_text: string;
+	encrypted_text: string;
+	decrypted_text: string;
+  	constructor(public navCtrl: NavController) {
+    	var text = "What are you doing?";
+	    var key = CryptoJS.enc.Hex.parse("882E91D56547F1CF7ED6BAAD9C3EAAF5");
+	    var iv  = CryptoJS.enc.Hex.parse("2811da22377d62fcfdb02f29aad77d9e");
+	    this.key = CryptoJS.enc.Hex.parse("882E91D56547F1CF7ED6BAAD9C3EAAF5");
+	    this.iv  = CryptoJS.enc.Hex.parse("2811da22377d62fcfdb02f29aad77d9e");
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+	    this.plain_text = '';
+	    this.encrypted_text = '';
+	    this.decrypted_text = '';
+
+	    // var decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+	    //                                       iv: iv,
+	    //                                       mode: CryptoJS.mode.CBC,
+	    //                                       padding: CryptoJS.pad.Pkcs7
+	    //                                     }).toString(CryptoJS.enc.Utf8);
+	    // console.log(decrypted);
+  	}
+
+  	encrypt(event): void {
+  		var text = this.plain_text;
+  		var encrypted = CryptoJS.AES.encrypt(text, this.key, {
+                iv: this.iv,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7
+              }).toString();
+
+	    this.encrypted_text = encrypted;
+  	}
+
+  	decrypt(event): void {
+  		var decrypted = CryptoJS.AES.decrypt(this.encrypted_text, this.key, {
+	                                          iv: this.iv,
+	                                          mode: CryptoJS.mode.CBC,
+	                                          padding: CryptoJS.pad.Pkcs7
+	                                        }).toString(CryptoJS.enc.Utf8);
+
+  		this.decrypted_text = decrypted;	    
+  	}
+
 
 }
