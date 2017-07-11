f47b218ddefd3288244b05c201e5aec42c4fa45f
jyyblue <jyyblue1987@outlook.com>
2017-07-12 00:42:09 +0800

    Revert "[add] add readme.txt"
    This reverts commit e9111b4092727b6b937f4934688a78ecbf852db8.

--- AESTest/src/app/app.module.ts
+++ AESTest/src/app/app.module.ts
@@ -7,9 +7,6 @@ import { TouchIdPage } from '../pages/touch-id/touch-id';
 import { PdfPage } from '../pages/pdf/pdf';
 import { PdfViewerComponent } from 'ng2-pdf-viewer';
 
-import { VotingPage } from '../pages/voting/voting';
-import { ResultsPage } from '../pages/results/results';
-
 import {HttpModule, Http, XHRBackend, RequestOptions} from '@angular/http';
 import {Events} from 'ionic-angular';
 import {httpFactory} from "../providers/http.factory";
@@ -21,8 +18,6 @@ import {httpFactory} from "../providers/http.factory";
     LoginPage,
     TouchIdPage,
     PdfPage,
-    VotingPage,
-    ResultsPage,
     PdfViewerComponent
   ],
   imports: [
@@ -35,8 +30,6 @@ import {httpFactory} from "../providers/http.factory";
     LoginPage,
     TouchIdPage,
     PdfPage,
-    VotingPage,
-    ResultsPage,
     PdfViewerComponent
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
